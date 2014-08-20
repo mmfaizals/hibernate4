ce0b18399e1beda2ae3f4b03307206a3425a428e
css95048 <mohamed.faizal@csscorp.com>
2014-08-20 14:26:55 +0530

    Read using HQL
    reads a record from person table using hql

--- src/test/java/css/training/hibernate/domain/Example1.java
+++ src/test/java/css/training/hibernate/domain/Example1.java
@@ -1,8 +1,11 @@
 package css.training.hibernate.domain;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Example1 {
@@ -19,6 +22,24 @@ public class Example1 {
 		ses.getTransaction().commit();
 		ses.close();
 		factory.close();
+	}
+
+	@Test
+	public void testRead() {
+		SessionFactory factory = sessionFactory();
+		Session ses = factory.openSession();
+		try {
+			ses.beginTransaction();
+			List<Person> persons = ses.createQuery(
+					"from Person where firstName='Nobita'").list();
+			for (Person it : persons) {
+				Assert.assertEquals(it.getFirstName(), "Nobita");
+			}
+			ses.getTransaction().commit();
+			ses.close();
+		} finally {
+			factory.close();
+		}
 	}
 
 	private static SessionFactory sessionFactory() {
